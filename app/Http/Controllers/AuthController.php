7f077f1b1fc0266b0490de2ff8206057ce4982e2
ebagabee <ebagabe.2025@gmail.com>
2025-05-04 17:11:47 -0300

    feat: implement user registration and authentication with Sanctum

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        $request->merge([
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user = User::create($request->validated());
+
+        $success['token'] = $user->createToken('chatApp')->plainTextToken;
+        $success['user'] = new UserResource($user);
+
+        return response()->json([
+            'user' => $success['user'],
+            'token' => $success['token']
+        ], 201);
+    }
 }
